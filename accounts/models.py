ea580c9dedf0479d9fe19451b5548b915a5a5674
Jakeline Freitas <jakefreaitas@gmail.com>
2021-02-02 19:47:13 -0300

    add app accounts

---
+++ accounts/models.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    email = models.EmailField('email', unique=True, blank=True)
+    # Booleano. Determina se este usuário pode acessar o site admin.
+    is_staff = models.BooleanField('staff status', default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name']
--- agenda_salao/settings.py
+++ agenda_salao/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #local apps
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# User Model
+AUTH_USER_MODEL = "accounts.User"
