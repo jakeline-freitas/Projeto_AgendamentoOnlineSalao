c0bdc6be5ba7e0b101d2385365875288a6a821a7
Jakeline Freitas <jakefreaitas@gmail.com>
2021-02-03 13:54:46 -0300

    commit

--- agenda_salao/settings.py
+++ agenda_salao/settings.py
@@ -77,14 +77,20 @@ WSGI_APPLICATION = 'agenda_salao.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+#DATABASES = {
+    #'default': {
+        #'ENGINE': 'django.db.backends.postgresql',
+        #'NAME': '',
+        #'USER': '',
+        #'PASSWORD': '',
+        #'HOST': 'localhost',
+        #'PORT': '5432',
+    #}
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'agendaSalao',
-        'USER': 'postgres',
-        'PASSWORD': 'annajulia',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
