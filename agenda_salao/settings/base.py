7e3a76c8e78a19f01222de39068bf752ef905b0c
Jakeline Freitas <jakefreaitas@gmail.com>
2021-03-23 13:14:25 -0300

    novas alterações

--- agenda_salao/settings/base.py
+++ agenda_salao/settings/base.py
@@ -180,6 +180,3 @@ EMAIL_HOST_PASSWORD = '760a1e1c2480a6'
 EMAIL_PORT = '2525'
 
 DISABLE_COLLECTSTATIC=1
-# Configure o aplicativo Django para Heroku.
-import django_heroku
-django_heroku.settings (locals ())
